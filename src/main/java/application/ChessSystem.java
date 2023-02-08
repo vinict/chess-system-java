e9e7409889424be2288a986de151d16ef5289d71
Vinicius Tavares <vinitavares.gomes@gmail.com>
2023-02-08 11:30:15 -0300

    Começando a implementar Board e Piece

--- src/main/java/application/ChessSystem.java
+++ src/main/java/application/ChessSystem.java
@@ -1,13 +1,14 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class ChessSystem {
 
     public static void main(String[] args) {
         
-        Position posi = new Position(3, 5);
+        Board board = new Board(8,8);
         
-        System.out.println(posi);
+        
     }
 }
