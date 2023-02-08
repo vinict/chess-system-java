300ea7fe5a373c2111332b03c212169ea780bf36
Vinicius Tavares <vinitavares.gomes@gmail.com>
2023-02-08 11:13:30 -0300

    Project created

---
+++ src/main/java/application/ChessSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package application;
+
+/**
+ *
+ * @author vinic
+ */
+public class ChessSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
