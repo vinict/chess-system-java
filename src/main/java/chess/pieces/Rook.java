83f88eb9be0e437387cd8a460db7a356c7dcdc66
Vinicius Tavares <vinitavares.gomes@gmail.com>
2023-02-08 17:47:57 -0300

    Movimentos possiveis de uma peça

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -15,4 +15,10 @@ public class Rook extends ChessPiece { /* Criação da peça TORRE */
         return "R";
     }
 
+    @Override
+    public boolean[][] possibleMoves(){
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
+    
 }
