223db1c0934d827fe6a7a7bd1b62c20e469467c8
Vinicius Tavares <vinitavares.gomes@gmail.com>
2023-02-08 16:20:11 -0300

    Tratando de algumas exceções e limpando a tela

--- src/main/java/application/ChessSystem.java
+++ src/main/java/application/ChessSystem.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 
 
 public class ChessSystem {
@@ -16,18 +18,29 @@ public class ChessSystem {
         chess.ChessMatch chessMatch = new ChessMatch();
         
         while(true){
-            UI.printBoard(chessMatch.getPieces()); /* User interface para receber a matriz de peças da partida e mostrá-la*/
-            
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces()); /* User interface para receber a matriz de peças da partida e mostrá-la*/
+
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch(ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }   
         
         
